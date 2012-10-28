2511c2ed65fd91a1ac795dd2ed6c2385d4b2d2d8
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-10-28 21:12:37 +0100

    Work on get_v function

--- R/get-v.r
+++ R/get-v.r
@@ -5,25 +5,61 @@
 #' @param name Variable name
 #' @param path Directory of where the variable is located
 #' @param dims A numeric or character vector specifying the dimension of the data (e.g. year and month)
+#' @param na Specification of how missing values should be coded
 #' 
 #' @export
 #'
-get_v <- function(name, path = getwd(), dims = NULL) {
+get_v <- function(name, path = getwd(), dims = NULL, na = NA) {
   
-  v_name <- file_name(name, dims)
-  v_dir <- file_dir(name, path, create_dir = FALSE)
-  v_path <- file.path(v_dir, v_name)  
+  # Get file path
+  cdb <- file_path(name, path, dims, ext = c("cdb.gz", "cdb"), create_dir = FALSE)
 
-  path_compressed <- paste(v_path, "cdb.gz", sep = ".")
-  path_uncompressed <- paste(v_path, "cdb", sep = ".")
-  
-  if (file.exists(path_compressed)) {
-    bin_file <- gzfile(path_compressed, "rb")
-  } else if (file.exists(path_uncompressed)) {
-    bin_file <- file(path_uncompressed, "rb")
+  # Connect to compressed/uncompressed file
+  if (file.exists(cdb[1])) {
+    bin_file <- gzfile(cdb[1], "rb")
+    
+  } else if (file.exists(cdb[2])) {
+    bin_file <- file(cdb[2], "rb")
+
   } else {
     cat("File does not exist\n")
     return(NULL)
   }
   
+  type <- rawToChar(readBin(bin_file, raw(), n = 1, size = 1, signed = FALSE))
+  bytes	<- readBin(bin_file, integer(), n = 1, size = 1, signed = FALSE)
+  exponent <- readBin(bin_file, integer(), n = 1, size = 1, signed = FALSE)
+  
+  version_len <- readBin(bin_file, integer(), n = 1, size = 8)
+  version_str <- rawToChar(readBin(bin_file, raw(), n = version_len))
+
+  attr_len <- readBin(bin_file, integer(), n = 1, size = 8)
+  attr_str <- rawToChar(readBin(bin_file, raw(), n = attr_len))  
+
+  levels_len <- readBin(bin_file, integer(), n = 1, size = 8)
+  levels_str <- rawToChar(readBin(bin_file, raw(), n = levels_len))  
+
+  vector_len <- readBin(bin_file, integer(), n = 1, size = 8) 
+  
+  if (bytes <= 4) {
+    x <- readBin(bin_file, integer(), n = vector_len, size = bytes)
+    #if (H_itemSize == 1) data[data == -128] 	<- as.integer(NA)
+    #if (H_itemSize == 2) data[data == -32768] 	<- as.integer(NA)
+  } else {	
+    x <- readBin(bin_file, double(), n = vector_len)
+  }
+
+  close(bin_file)
+  
+  if (type == "i") {
+    if(!is.na(na)) x[is.na(x)] <- as.integer(na)
+  } else if (type == "d") {
+    if(exponent > 0)	x <- x / 10.0^exponent
+    if(!is.na(na)) x[is.na(x)] <- as.double(na)
+  } else if(type == "l") {
+    x <- (x > 0L)
+    if(!is.na(na)) x[is.na(x)] <- as.logical(na)
+  }
+  
+  return(x)
 }
