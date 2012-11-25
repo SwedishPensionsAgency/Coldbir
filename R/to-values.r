19f0c99a579aa1a4d01bfbed3631aa4896b973f1
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-11-25 18:06:01 +0100

    Change some names

--- R/to-values.r
+++ R/to-values.r
@@ -3,12 +3,16 @@
 #' Function to replace keys with values in a variable vector
 #'
 #' @param v Vector
-#' @param ht Hash table
+#' @param df Two-column data frame with keys and values
 #' @param factors If vector should be returned as factors, default is FALSE.
 #'
 #' @export
 #'
-key_to_value <- function(v, ht, factors = FALSE) {
+to_values <- function(v, df, factors = FALSE) {
+    
+    if (!is.data.frame(df) || ncol(df) != 2) 
+        stop("Input must be a two-column data frame")
+    
     v <- ht[[2]][match(v, ht[[1]])]
     if (!factors) 
         v <- as.character(v)
