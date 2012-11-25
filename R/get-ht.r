d97d7f2b5417228fdb0328569a613518e9cd78d8
Thomas Reinholdsson <reinholdsson@gmail.com>
2012-11-25 11:34:30 +0100

    Add functionality for reading hash tables

---
+++ R/get-ht.r
@@ -0,0 +1,14 @@
+#' Read hash table from disk
+#'
+#' Read a hash table (dictionary) that represents variable data to disk.
+#'
+#' @param name Variable name
+#' @param path Directory of where the file is located
+#'
+#' @export
+#'
+get_ht <- function(name, path = getwd()) {
+    folder_path <- file_path(name, path, create_dir = FALSE, file_name = FALSE)
+    df <- read.table(file = file.path(folder_path, "dictionary.tsv"), header = TRUE, quote = "", sep = "\t")
+    return(df)
+} 
--- R/put-ht.r
+++ R/put-ht.r
@@ -1,6 +1,6 @@
-#' Save hash table to disk
+#' Write hash table to disk
 #'
-#' Used to save a hash table (dictionary) that represents variable data to disk.
+#' Write a hash table (dictionary) that represents variable data to disk.
 #'
 #' @param name Variable name
 #' @param path Directory of where the file are to be created
@@ -9,7 +9,7 @@
 #'
 #' @export
 #'
-put_ht <- function(name, path = getwd(), key, value, create_dir = TRUE) {
+put_ht <- function(key, value, name, path = getwd(), create_dir = TRUE) {
     
     folder_path <- file_path(name, path, create_dir = create_dir, file_name = FALSE)
     
