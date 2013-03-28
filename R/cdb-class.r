f439169006199dce4eda7cd5dcc2f5589ff24864
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-28 23:35:32 +0100

    Remove help server from package

--- R/cdb-class.r
+++ R/cdb-class.r
@@ -142,20 +142,6 @@ setMethod(
     }
 )
 
-#' Help method: a?"foo"
-#' 
-#' @export
-setMethod("?",  c("cdb", "character"), function(e1, e2) {
-    run_help(path = e1@path, search = e2)
-})
-
-#' Help method: ?a
-#' 
-#' @export
-setMethod("?",  "cdb", function(e1) {
-    run_help(path = e1@path)
-})
-
 #' List all variables in database
 #' 
 #' ...
