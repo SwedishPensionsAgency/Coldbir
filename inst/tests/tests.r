ea26376fe4727c9e86ec66ebcc8e7527f6c9eeba
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-04-29 17:39:24 +0200

    Add tests

--- inst/tests/tests.r
+++ inst/tests/tests.r
@@ -1,6 +1,6 @@
 # devtools::test(".")
 
-path <- "temp"
+path <- tempfile()
 size <- 100
 
 context("init database")
@@ -11,22 +11,31 @@ context("init database")
 
 context("variable (integer)")
     x <- sample(c(0, 1, 10000, .Machine$integer.max, NA), size, replace = TRUE)
-    test_that("put variable", {
-        expect_message(db["x"] <- x)
-    })
+    db["integer"] <- x
+
     test_that("get variable", {
         expect_error(db["non-existing"])
-        expect_equal(x, db["x"])
+        expect_equal(x, db["integer"])
     })
 
 context("variable (double)")
     x <- sample(c(-100, -50, 0, 50, 100, NA), size, replace = TRUE)
-    test_that("put variable", {
-        expect_message(db["x"] <- x)
-    })
+    db["double"] <- x
+
     test_that("get variable", {
-        expect_error(db["non-existing"])
-        expect_equal(x, db["x"])
+        expect_equal(x, db["double"])
+    })
+
+context("variable (POSIXct)")
+    ts <- as.character(.POSIXct(runif(1e3) * unclass(Sys.time())))
+    # ts <- as.character(as.POSIXct(runif(1e4) * unclass(Sys.time()), origin = "1970-01-01"))
+
+    #attr(x, "tzone") <- "GMT"
+    x <- as.POSIXct(ts)
+    #x_chr <- as.character(x)
+    db["POSIXct"] <- x
+    test_that("get variable", {
+        expect_equal(as.character(x), as.character(db["POSIXct"]))
     })
 
 # Remove all temporary test files
