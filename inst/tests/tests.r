4e0d7c9e9a8bfc4883e8a1e7b4aada9a238d5652
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-04-29 23:50:16 +0200

    Update tests

--- inst/tests/tests.r
+++ inst/tests/tests.r
@@ -12,7 +12,6 @@ context("init database")
 context("variable (integer)")
     x <- sample(c(0, 1, 10000, .Machine$integer.max, NA), size, replace = TRUE)
     db["integer"] <- x
-
     test_that("get variable", {
         expect_error(db["non-existing"])
         expect_equal(x, db["integer"])
@@ -21,18 +20,12 @@ context("variable (integer)")
 context("variable (double)")
     x <- sample(c(-100, -50, 0, 50, 100, NA), size, replace = TRUE)
     db["double"] <- x
-
     test_that("get variable", {
         expect_equal(x, db["double"])
     })
 
 context("variable (POSIXct)")
-    ts <- as.character(.POSIXct(runif(size) * unclass(Sys.time())))
-    # ts <- as.character(as.POSIXct(runif(1e4) * unclass(Sys.time()), origin = "1970-01-01"))
-
-    #attr(x, "tzone") <- "GMT"
-    x <- as.POSIXct(ts)
-    #x_chr <- as.character(x)
+    x <- .POSIXct(runif(size) * unclass(Sys.time()))
     db["POSIXct"] <- x
     test_that("get variable", {
         expect_equal(as.character(x), as.character(db["POSIXct"]))
