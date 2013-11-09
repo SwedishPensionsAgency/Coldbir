73b247ff8f6e87dd2a9bc3a7ea16dfec899cff5a
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-11-09 13:04:46 +0100

    add docs

--- R/cdb-class-r5.r
+++ R/cdb-class-r5.r
@@ -1,3 +1,19 @@
+#' Assign new (or existing) coldbir database
+#' 
+#' Method to assign either a new or existing coldbir database to an R object.
+#' The current working directory is set as the default path.
+#' 
+#' @param path Database path (the location of the coldbir database)
+#' @param dims Set default dimensions
+#' @param type Return type of variable. Possible values: 'c' = character, 'f' = factor and 'n' = numeric (default).
+#' Character conversion might be a bit slow; hence numeric or factor is recommended.
+#' @param na Value representing missing values (default: NA_real_)
+#' @param log_level Log level (default: 4). Available levels: 1-9.
+#' @param log_file Log file. As default log messages will be written to console.
+#' 
+#' @examples db <- cdb()
+#' @export
+#'
 cdb2 <- setRefClass(
   "cdb2",
   fields = list(
@@ -5,7 +21,8 @@ cdb2 <- setRefClass(
     type = "character",
     na = "numeric",
     log_level = "numeric",
-    log_file = "character"
+    log_file = "character",
+    dims = "ANY"
   ),
   methods = list(
     initialize = function(
@@ -13,7 +30,8 @@ cdb2 <- setRefClass(
         type = "f",
         na = NA_real_, 
         log_level = 4,
-        log_file = ""
+        log_file = "",
+        dims = NULL
       ) {
       
       # Validate
@@ -23,11 +41,12 @@ cdb2 <- setRefClass(
       }
       
       # Set parameters
-      path <<- path
-      type <<- type
-      na <<- na
-      log_level <<- log_level
-      log_file <<- log_file
+      .self$path <- path
+      .self$type <- type
+      .self$na <- na
+      .self$log_level <- log_level
+      .self$log_file <- log_file
+      .self$dims <- dims
       
       # Set futile.logger options
       flog.threshold(log_level)
@@ -48,11 +67,13 @@ setMethod(
   signature = "cdb2",
   definition = function(x, i, j){
     
-    if (missing(j)) j <- NULL
+    if (missing(j)) j <- x$dims
     
     if (missing(i) || is.vector(i) && length(i) > 1){
       if (missing(i)){
-        i <- get_vars(x, dims = FALSE)
+        vars <- get_vars(a, dims = T)
+        fun <- function(x) isTRUE(all.equal(x, as.character(j)))
+        i <- vars[sapply(vars$dims, fun)]$variable
       }
       
       # Create data.table with first variable
@@ -60,17 +81,19 @@ setMethod(
       setnames(v, "first", i[1])
       
       # Add all other variables
-      for(var in i[2:length(i)]){
-        v[ , var := x[var, j], with = F]
+      if (length(i) > 1) {
+        for(var in i[2:length(i)]){
+          v[ , var := x[var, j], with = F]
+        }
       }
       
     } else {
-      
       v <- get_variable(name = i, path = x$path, dims = j, na = x$na)
       
       # Convert to character or factor (if requested)
       if (x$type %in% c("c", "f")) {
-        v <- to_char(x = v, name = i, path = x$path, factors = x$type == "f")
+        factors <- if (x$type == "f") TRUE else FALSE
+        v <- to_char(x = v, name = i, path = x$path, factors = factors)
       }
     }
     
@@ -84,8 +107,7 @@ setMethod(
   f = "[<-",
   signature = "cdb2",
   definition = function(x, i, j, value){
-    
-    if (missing(j)) j <- NULL
+    if (missing(j)) j <- x$dims
     
     if (all(class(value) == "doc")) {
       
@@ -99,3 +121,5 @@ setMethod(
     return(x)
   }
 )
+
+
