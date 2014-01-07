db115eea22a7fc99fbb9c60ca2e04d7b53fcb838
Thomas Reinholdsson <thomas.reinholdsson@pensionsmyndigheten.se>
2014-01-07 11:15:02 +0100

    [#67] Update roxygen docs

--- R/messages.r
+++ R/messages.r
@@ -1,5 +1,24 @@
-msg <- wrn <- err <- function(n, ...) {
+#' Log messages
+#' 
+#' Handlers for showing messages, warnings and errors.
+#' These functions are useful as many of the log messages
+#' are re-used in the code. `msg()` is used for ordinary messages,
+#' `wrn()` for warnings and `err()` for throwing an error and
+#' interrupting the code from further evaulation.
+#' 
+#' @param n message code. The number used must exist in table available in the code.
+#' @param ... arguments passed on to `sprintf()`.
+#' 
+#' @examples 
+#' \dontrun{
+#' msg(3, "foo")
+#' }
+#' 
+#' @rdname messages
+msg <- function(n, ...) {
   
+  # Message codes are available in the table below;
+  # some numbers in between have been removed since they aren't used anymore.
   str <- sprintf(switch(
     as.character(n),  # obs: convert to char since int otherwise refers to index
     "0"   = "Error message is not defined!",
@@ -7,17 +26,13 @@ msg <- wrn <- err <- function(n, ...) {
     "2"   = "%s - writing failed; rollback! (%s)",
     "3"   = "%s - no such data base",
     "4"   = "%s : %s - no documantation for this variable",
-  # "5"   = "File path: %s",
     "6"   = "%s - file does not exist",
     "7"   = "%s - version of coldbir package and file format does not match",
     "8"   = "You're only allowed to read data, to change this use db$read_only <- F",
     "9"   = "%s - variable is NULL; nothing to write",
-  # "10"  = "%s - all values are missing",
     "11"  = "%s - length of variable doesn't match the size of the other columns; nothing will be written",
-  # "12"  = "%s - character converted to factor",
     "13"  = "%s - data type is not supported",
     "14"  = "%s - writing failed; rollback! (%s)",
-  # "15"  = "Success: %s",
     "16"  = "input must be a two-column data frame"
   ), ...)
   
@@ -30,3 +45,9 @@ msg <- wrn <- err <- function(n, ...) {
     "err" = stop(str)
   )
 }
+
+#' @rdname messages
+wrn <- msg
+
+#' @rdname messages
+err <- msg
